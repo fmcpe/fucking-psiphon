7283082633ab6129641d6604a8b2a643aebebcfb
Serenity <70578351+fmcpe@users.noreply.github.com>
2024-05-06 11:32:03 +0700

    Update main.go

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/fmcpe/fucking-psiphon/src/libpsiphon"
+	"github.com/fmcpe/fucking-psiphon/libpsiphon"
 	"github.com/aztecrabbit/libinject"
 	"github.com/aztecrabbit/liblog"
 	"github.com/aztecrabbit/libproxyrotator"
