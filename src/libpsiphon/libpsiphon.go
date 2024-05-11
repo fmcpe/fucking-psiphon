3215b108ed0681ebc7fefbfc44e8da12b7041ece
Serenity <70578351+fmcpe@users.noreply.github.com>
2024-05-11 17:23:26 +0700

    Update libpsiphon.go

--- src/libpsiphon/libpsiphon.go
+++ src/libpsiphon/libpsiphon.go
@@ -141,7 +141,7 @@ func (p *Psiphon) Start() {
 		PsiphonData.TargetServerEntry = p.Config.TargetServerEntry
 	}
 
-	p.LogInfo(strings.TrimSpace(p.Config.TargetServerEntry),liblog.Colors["R1"])
+	p.LogInfo(len(strings.TrimSpace(p.Config.TargetServerEntry)),liblog.Colors["R1"])
 
 	libutils.JsonWrite(PsiphonData, PsiphonData.MigrateDataStoreDirectory+"/config.json")
 
